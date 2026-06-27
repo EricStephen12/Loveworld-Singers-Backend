b15d8d34b25d1872145876193f351525b90edc5b
EricStephen12 <deamirclothingstores@gmail.com>
2026-06-27 15:59:56 +0100

    feat: configure deep linking support by adding app association files and setting response headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
